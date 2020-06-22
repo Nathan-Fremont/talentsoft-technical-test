51f7b3c9d4da3e5642bb9a1fca39f03abc5a99fe
Yoliser <nethlarion@live.fr>
2020-06-22 14:24:50 +0200

    First commit with Android code.

---
+++ app/src/main/java/com/example/talentsoft_technical_test/ui/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.talentsoft_technical_test.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.talentsoft_technical_test.R
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
